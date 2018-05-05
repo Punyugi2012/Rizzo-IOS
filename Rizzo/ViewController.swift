63e232ea59ade8998667fb194f848b8c45f113e9
Punyugi2012 <58160216@go.buu.ac.th>
2018-05-05 17:50:17 +0700

    Initial Commit

---
+++ Rizzo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rizzo
+//
+//  Created by punyawee  on 5/5/61.
+//  Copyright © พ.ศ. 2561 punyawee . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
